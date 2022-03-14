c1ed7b573e131544ce6a2f9ba546319d6847a560
santiagogui1909 <santiagogui1909@gmail.com>
2022-03-14 12:06:09 -0500

    eliminacion extension devtools

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,11 +1,10 @@
 import {loginReducer} from '../Reducer/loginReducer';
 import {movieReducer} from '../Reducer/movieReducer';
 import {createStore , combineReducers} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
 
 const reducers = combineReducers({
     loginReducer,
     movieReducer,
 });
   
-export const store = createStore(reducers , composeWithDevTools());
+export const store = createStore(reducers);
